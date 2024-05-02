2ef9584ee2797d0a46277ee5cce1dc26e0f2cab5
sanduni-sc <sanduni@saras.care>
2024-05-02 12:34:10 +1000

    venv updates

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sachinwedasinghe/Documents/GitHub/sarascare_py/sarascare_py/venv"
+set -gx VIRTUAL_ENV "/Users/sachinwedasinghe/Documents/GitHub/sarascare_py/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

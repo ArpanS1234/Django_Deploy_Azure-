2e598f810932520ec14706a3bdaa3f7cbf4e00cb
ArpanS1234 <arpan@saras.care>
2024-05-05 00:01:14 +1000

    update settings.py

--- sarascare_py/settings.py
+++ sarascare_py/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-63gddvop@%n4)hhk$-^5+he%#jdd6o(#rqa-mq5jsv#^esuukw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False   
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoazureservice.azurewebsites.net']
 
 # Application definition
 

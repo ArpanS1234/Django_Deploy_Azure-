4a13a08fd4e8a8bbb437fc8b13e50897ec3c5778
ArpanS1234 <arpan@saras.care>
2024-05-05 11:10:58 +1000

    Update settings.py

--- sarascare_py/settings.py
+++ sarascare_py/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifeststaticFilesstorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'sarascare_py.urls'
 
 TEMPLATES = [
